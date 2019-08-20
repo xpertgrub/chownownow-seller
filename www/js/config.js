08ad2c705921849d94d3bf0610f30fdafe0cee38
Xpert Grub <xpertgrub@users.noreply.github.com>
2019-08-20 13:17:52 +0000

    update

--- www/js/config.js
+++
@@ -1,7 +0,0 @@
-
-var krms_config ={					
-	'ApiUrl':"https://www.chownownow.com/merchantapp/api",
-	'DialogDefaultTitle':"Chow Now Now Seller",
-	'APIHasKey':"74cbafdf48703d2d98b5f98b1335cd8b",
-	'debug': false
-};
